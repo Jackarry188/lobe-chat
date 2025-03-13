17fbdffacd598d26924d37fc91a0f170531389bc
Jack <skyy_ok@126.com>
2025-03-13 19:33:15 +0800

    Update index.tsx
    Signed-off-by: Jack <skyy_ok@126.com>

--- src/features/MobileSwitchLoading/index.tsx
+++ src/features/MobileSwitchLoading/index.tsx
@@ -7,7 +7,7 @@ import { Center, Flexbox } from 'react-layout-kit';
 
 import { ProductLogo } from '@/components/Branding';
 
-// @ts-expect-error
+
 const MobileSwitchLoading: any = memo(() => {
   const { t } = useTranslation('common');
   return (
