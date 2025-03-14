d38cd85c80c08cafd8a56df0e98ae67aa4859c47
yuanliting <18236864545@163.com>
2025-03-14 17:55:51 +0800

    打印process.env.NEXT_PUBLIC_SERVICE_MODE

--- src/const/version.ts
+++ src/const/version.ts
@@ -6,6 +6,8 @@ import { BRANDING_NAME, ORG_NAME } from './branding';
 export const CURRENT_VERSION = pkg.version;
 
 export const isServerMode = getServerDBConfig().NEXT_PUBLIC_ENABLED_SERVER_SERVICE;
+console.log('11isServerMode', isServerMode);
+console.log('process.env.NEXT_PUBLIC_SERVICE_MODE', process.env.NEXT_PUBLIC_SERVICE_MODE);
 export const isUsePgliteDB = process.env.NEXT_PUBLIC_CLIENT_DB === 'pglite';
 
 export const isDeprecatedEdition = !isServerMode && !isUsePgliteDB;
